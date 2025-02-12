d5f93e1482657ef14ad5fd65265c5c73501a9091
sushilK2099 <sushil.k@projectx38.com>
2025-02-12 23:08:40 +0530

    inventory

--- app/controllers/pageController.js
+++ app/controllers/pageController.js
@@ -7,8 +7,13 @@ var controllers = {
     index: async function (req, res) {
       const order = await DataServices.getGetData("twocliq-dashboard/orders/details", "testToken", {"orderId" : "TR0000003"})
       const data = order.data
-      const products = await DataServices.getGetData("/twocliq-dashboard/products/activeList", "testToken", {})
-      return res.render('pages/order', {data:data, products : products.data});
+      return res.render('pages/order', {data:data});
+    },
+
+    confirm: async function (req, res) {
+      const order = await DataServices.getGetData("twocliq-dashboard/orders/details", "testToken", {"orderId" : "TR0000003"})
+      const data = order.data
+      return res.render('pages/confirmOrder', {data:data});
     },
 
     upload : async function (req, res) {
--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,8 @@ var pageController = require('./controllers/pageController');
 
 module.exports = function(app) {
     app.route('/').get(pageController.index);
+    app.route('/confirm').get(pageController.confirm);
+
     app.route('/fileUpload').get(pageController.upload)
     app.route('/fileUpload').post(pageController.uploadBackend)
  }
